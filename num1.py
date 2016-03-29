d349a7608847063037a569f55b4dae8b2fc40754
mogu <melancholicrainxx@gmail.com>
2016-03-29 14:48:06 +0000

    number

---
+++ num1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# 四則演算
+
+test_integer = 100
+
+print (test_integer + 10)   # 加算
+print (test_integer - 10)   # 減算
+print (test_integer * 10)   # 乗算
+print (test_integer / 10)   # 除算
