4cf53b36d10b23939c0f46343132b5922dc0612b
mogu <melancholicrainxx@gmail.com>
2016-04-02 15:11:12 +0000

    list

--- list/list1.py
+++ list/list1.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+
+test_list_1 = ['python','-','izm','-','com']
+print(test_list_1)
+
+print(test_list_1[2])
+
+
+for i in test_list_1:
+    print(i)
+
+print(range(0,6))
+
+    
+#for i in range(0, 5):
+ #   print("%s回目のループだよ！" % i)
+    
