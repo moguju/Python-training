77e462897e47ed2a1c1d8253712e50e521d82d95
moguju <melancholicrainxx@gmail.com>
2016-04-03 14:18:04 +0000

    list

---
+++ list/list2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+test_list_1 = []
+print(test_list_1)
+
+
+test_list_1.append('python')
+test_list_1.append('-')
+test_list_1.append('izm')
+test_list_1.append('.')
+test_list_1.append('com')
+
+print(test_list_1)
