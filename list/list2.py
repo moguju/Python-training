5d9e8fed4b37dc10c60f7f16ead11a4a5007803a
moguju <melancholicrainxx@gmail.com>
2016-04-03 15:19:12 +0000

    list

--- list/list2.py
+++ list/list2.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
-test_list_1 = []
+test_list_1 = ["Hello"]
 print(test_list_1)
 
 
 test_list_1.append('python')
+print(test_list_1)
 test_list_1.append('-')
+print(test_list_1)
 test_list_1.append('izm')
+print(test_list_1)
 test_list_1.append('.')
+print(test_list_1)
 test_list_1.append('com')
-
 print(test_list_1)
