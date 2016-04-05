717afbac91f402466b861dc6c9a7a7a3f3dfe52c
moguju <melancholicrainxx@gmail.com>
2016-04-05 10:08:38 +0000

    list

---
+++ list/list3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import sys
+
+test_list_1 = ['python','izm','com']
+print(test_list_1)
+
+print("")
+
+test_list_1.insert(1,'-')
+test_list_1.insert(3,'.')
+
+print(test_list_1)
+
+test_list_1.insert(0,'http://www.')
+
+print(test_list_1)
+
+for i in test_list_1:
+    sys.stdout.write(i)
